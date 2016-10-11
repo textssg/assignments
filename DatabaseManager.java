55f12dca9477ccff2e9d30140a882e6f155a2468
textssg <textssg@gmail.com>
2016-10-11 23:18:30 +0530

    Add files via upload

---
+++ DatabaseManager.java
@@ -0,0 +1,73 @@
+package com.airvoice.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class DatabaseManager {
+
+	private String dbURL;
+	private String userName;
+	private String password;
+	private String driver;
+	private Connection connection;
+	
+	public DatabaseManager(String dbURL,String userName,String password,
+			String driver){
+		this.dbURL=dbURL;
+		this.userName=userName;
+		this.password=password;
+		this.driver=driver;
+	}
+	
+	public void open() /*throws exception*/{
+		try {
+			System.out.println(this.driver);
+			Class.forName(driver);
+			connection=DriverManager.getConnection(dbURL,userName,password);
+			System.out.println("Database has been connected");
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+	
+	public void close(){
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	public void insertIntoTable(String tableName, HashMap<String,String> fieldNameValue){
+		StringBuffer sb=new StringBuffer();
+		sb.append("insert into "+tableName+" ( ");
+		 Iterator it = fieldNameValue.entrySet().iterator();
+	      while(it.hasNext()) {
+	         Map.Entry pair = (Map.Entry)it.next();
+	         sb.append(pair.getKey());
+	         if(it.hasNext()) {
+	           sb.append(", ");
+	         }
+	      }
+	    sb.append(" ) Values ( ");
+	    it=fieldNameValue.entrySet().iterator();
+	    while(it.hasNext()){
+	    	Map.Entry pair= (Map.Entry)it.next();
+	    	sb.append(pair.getKey());
+	    	if(it.hasNext()){
+	    		sb.append(", ");
+	    	}
+	    }
+	    sb.append(" ) ; ");
+	    try {
+			Statement s=connection.createStatement();
+			s.executeUpdate(sb.toString());
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
